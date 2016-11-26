a07dbec132df0d01f2e2fb20a0c4b21ea3475b65
meikaik <meikaikoh@gmail.com>
2016-11-25 23:57:15 -0500

    started on main.cc

--- main.cpp
+++ main.cpp
@@ -7,8 +7,22 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "readconsole.hpp"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
+    int seed = 0;
+    int startLevel = 0;
+    bool textMode;
+    string scriptFile{"sequence.txt"};
+    string argInput;
+    for (int i = 0; i < argc; i++) {
+        argInput = argv[i];
+        
+    }
+    
     
     return 0;
 }
